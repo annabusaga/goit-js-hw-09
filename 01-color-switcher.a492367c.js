a2886435a47613d8adaa1c2bc59e7a4e9afd3427
annabusaga <113771217+annabusaga@users.noreply.github.com>
2023-02-27 09:51:49 +0000

    Deploying to gh-pages from @ annabusaga/goit-js-hw-09@0357636200e76974aba3f43e575efb16b6efb937 🚀

---
+++ 01-color-switcher.a492367c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a=null;e.disabled=!0,t.addEventListener("click",(function(t){a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.target.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(e){clearInterval(a),t.disabled=!1,e.target.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.a492367c.js.map
